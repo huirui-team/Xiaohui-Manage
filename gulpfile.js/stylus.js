03026ad5d5cc0a83e23c5e301bb49fd6cf41660b
bluecloudi <1847315825@qq.com>
2019-10-05 11:28:26 +0800

    change stylus.js

--- gulpfile.js/stylus.js
+++ gulpfile.js/stylus.js
@@ -1,12 +1,10 @@
 var gulp         = require('gulp');
 var stylus       = require('gulp-stylus');
 var nib          = require('nib');
-
-function stylus(){
+gulp.task('stylus', function () {
     gulp.src('stylusExample/src/*.styl')
         .pipe(stylus(
             { use: [nib()] }  /* 直接调用nib，就可以一并编译 */
         ))
         .pipe(gulp.dest('css/'))
-}
-exports.stylus=stylus;
+});
